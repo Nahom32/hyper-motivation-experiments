e88606da7a109736a8b870ba44803daf1c9cf3b2
Nahom32 <senaynahom00@gmail.com>
2025-05-09 09:28:53 +0300

    feat: added action-utils

--- action-planning/action-utils.py
+++ action-planning/action-utils.py
@@ -3,7 +3,7 @@ import random
 import math
 
 
-def shannon_entropy(s,c,tv, prior_a: float = 1.0, prior_b: float = 1.0) -> float:
+def shannon_entropy(s,c,count,mean, prior_a: float = 1.0, prior_b: float = 1.0) -> float:
     """Return the shannon entropy of `atom`.
 
     The shannon entropy is calculated based on the mean of the beta
@@ -17,5 +17,24 @@ def shannon_entropy(s,c,tv, prior_a: float = 1.0, prior_b: float = 1.0) -> float
         return 0
 
     # Otherwise, calculate the actual Shannon entropy
-    mean = tv_to_beta(tv, prior_a, prior_b).mean()
+    mean = tv_to_beta(count,mean, prior_a, prior_b).mean()
     return st.entropy([mean, 1.0 - mean], base=2)
+
+def tv_to_beta(
+    count,mean, prior_a: float = 1.0, prior_b: float = 1.0
+) -> st.rv_continuous:
+    """Convert a truth value to a beta distribution.
+
+    Given a truth value, return the beta distribution that best fits
+    it.  Two optional parameters are provided to set the prior of the
+    beta-distribution, the default values are prior_a=1 and prior_b=1
+    corresponding to the Bayesian prior.
+
+    """
+
+   
+    pos_count = count * mean  # the mean is actually the mode
+    a = prior_a + pos_count
+    b = prior_b + count - pos_count
+    return st.beta(a, b)
+
