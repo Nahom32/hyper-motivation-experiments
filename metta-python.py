2fe60f0203024879fb7ef64e1137b7f8304d30a8
Nahom32 <senaynahom00@gmail.com>
2025-05-23 12:05:38 +0300

    feat: added metta-python

---
+++ metta-python.py
@@ -0,0 +1,27 @@
+from hyperon import MeTTa
+from numpy import random
+
+metta = MeTTa()
+
+# metta.run('''
+#     (data w x)
+#     (data a b)
+#     (w d x)
+#     !(collapse (match &self (data $x $y) ($x $y)))
+# ''')
+# pattern = '$x'
+# #print(result.pop())
+# print(metta.space().query(f'{pattern}'))
+query_var = metta.parse_all('$x').pop()
+for i in range(10):
+    rnd_num = random.rand()
+    atom = metta.parse_single(f'{rnd_num}')
+    
+    metta.space().add_atom(atom)
+query_result = metta.space().query(query_var)
+print(query_result)
+print(type(query_result))
+
+
+    
+
