1580e6b0b895287f684d092b377dc637811b384d
Nahom32 <senaynahom00@gmail.com>
2025-05-28 14:06:39 +0300

    feat: added space-comparison

--- monitor-changes.py
+++ monitor-changes.py
@@ -1,7 +1,14 @@
 from numpy import random
 from time import sleep
-def monitor_changes(func, atomspace):
+from hyperon import E,S,V,MeTTa
+def loop(func, atomspace):
     while True:
         wait = random.randint(2,5)
         sleep(wait)
         func(atomspace)
+
+
+cache = []
+curr = []
+def monitor_changes(atomspace:str):
+    pass
