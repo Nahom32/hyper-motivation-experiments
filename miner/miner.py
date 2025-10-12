5f8ab05fc1aa4679d93ab04a831d90fec6908a2f
Nahom32 <senaynahom00@gmail.com>
2025-10-12 09:19:22 +0300

    added types for miner emulator function

--- miner/miner.py
+++ miner/miner.py
@@ -1,6 +1,6 @@
 
-import google.generativeai as genai
-from dotenv import load_dotenv
+#import google.generativeai as genai
+#from dotenv import load_dotenv
 from typing import List
 import re
 import os 
